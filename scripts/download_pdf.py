fa0deb9b6c7203542b509ab9da80b54de44eac6b
chrisluo5311 <chrislo5311@gmail.com>
2025-05-29 00:27:10 -0700

    update download pdf function with absolute path

--- scripts/download_pdf.py
+++ scripts/download_pdf.py
@@ -1,7 +1,17 @@
 import requests
+import os
 
-def download_arxiv_pdf(arxiv_id: str, save_path: str) -> bool:
+current_dir = os.path.dirname(os.path.abspath(__file__)) # /Users/luojidong/程式/arxiv-copilot/scripts
+pdfs_dir = os.path.join(current_dir, "pdfs")
+
+def download_arxiv_pdf(arxiv_id: str) -> bool:
+    save_path = os.path.join(pdfs_dir, f"{arxiv_id}.pdf")
+    # check if the pdfs directory exists, if not, create it
+    if not os.path.exists(pdfs_dir):
+        os.makedirs(pdfs_dir)
+
     url = f"https://arxiv.org/pdf/{arxiv_id}.pdf"
+    print("Downloading PDF from:", url)
     response = requests.get(url)
     if response.status_code == 200:
         with open(save_path, "wb") as f:
